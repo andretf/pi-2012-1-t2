049b2f304a01ed4a08ad84c7e4855e005305b8d5
bsflores <bsflores@gmail.com>
2012-07-03 05:08:51 +0000

    separaBlocos

---
+++ separaBlocos.m
@@ -0,0 +1,20 @@
+% Devolve os blocos 8x8 da imagem
+function retorno = separaBlocos(I)
+	qtdBlocos = size(I, 1) / 8; % lenna é 512x512... então qtdBlocos por linha = qtdBlocos por coluna.
+	retorno = zeros(8, 8, qtdBlocos);
+	bloco = zeros(8, 8);
+	i = 1; % contador
+
+	% calcula as coordenadas em pixeis de um bloco – limites do bloco
+	for x = 0 : (qtdBlocos - 1)
+ 	  xFrom = x * 8 + 1;
+   	xTo = x * 8 + 8;
+   	for y = 0 : (qtdBlocos - 1)
+      	yFrom = y * 8 + 1;
+      	yTo = y * 8 + 8;
+      	retorno( :, :, i) = I(xFrom:xTo, yFrom:yTo);
+	   	i = i + 1;
+   	end
+   end
+   
+return
