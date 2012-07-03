28c3ff60586d2100e35fe05e39ae8b96a791b55f
andretf <andretf@gmail.com>
2012-07-03 11:23:30 +0000

    código Huffman => RunLength

--- jpeg_compacta.m
+++ jpeg_compacta.m
@@ -4,123 +4,55 @@
 
 %% onde I é a imagem e q é o fator multiplicativo q >= 1
 function jpeg = jpeg_compacta(I, q)
-	% imagem original
+% imagem original
 
-	% jpeg = Img retorno
-	% 1. Subtraia 128 do valor de cada pixel da imagem
-	jpeg = double(I);
-	jpeg = jpeg - 128;
+% jpeg = Img retorno
+% 1. Subtraia 128 do valor de cada pixel da imagem
+jpeg = double(I);
+jpeg = jpeg - 128;
 
-	% 2. Aplique a transformada DCT 2D em blocos de 8x8
-	% jpg = dct2(jpg);
-	% figure, imshow(J, 'Após DCT2');
+% 2. Aplique a transformada DCT 2D em blocos de 8x8
+% jpg = dct2(jpg);
+% figure, imshow(J, 'Após DCT2');
 
-	% 3  Tabela de Quantização dos coeficientes da DCT usando o multiplicador passado
-	Q = q .* [...
-		16 11 10 16 24 40 51 61; ...
-   	12 12 14 19 26 58 60 55; ...
-   	14 13 16 24 40 57 69 56; ...
-   	14 17 22 29 51 87 80 62; ...
-   	18 22 37 56 68 109 103 77; ...
-   	24 35 55 64 81 104 113 92; ...
-   	49 64 78 87 103 121 120 101; ...
-   	72 92 95 98 112 100 103 99];
+% 3  Tabela de Quantização dos coeficientes da DCT usando o multiplicador passado
+Q = q .* [...
+      16 11 10 16 24 40 51 61; ...
+      12 12 14 19 26 58 60 55; ...
+      14 13 16 24 40 57 69 56; ...
+      14 17 22 29 51 87 80 62; ...
+      18 22 37 56 68 109 103 77; ...
+      24 35 55 64 81 104 113 92; ...
+      49 64 78 87 103 121 120 101; ...
+      72 92 95 98 112 100 103 99];
 
-	% cria blocos 8x8
-	G = separaBlocos(jpeg);
-	DC = [];
-	AC = [];
+% cria blocos 8x8
+G = separaBlocos(jpeg);
+DC = [];
+AC = [];
 
-	for x = 1 : size(G, 3)
-		% aplica DCT2 pra cada um dos blocos
-		G(:, :, x) = dct2(G(:, :, x));
+for x = 1 : size(G, 3)
+   % aplica DCT2 pra cada um dos blocos
+   G(:, :, x) = dct2(G(:, :, x));
    
-		% aplica quantização em cada um dos elementos dos blocos
-		% Bj,k = round(Gj,k / Qj,k)
-   	G(:, :, x) = round(G(:, :, x)./Q);
+   % aplica quantização em cada um dos elementos dos blocos
+   % Bj,k = round(Gj,k / Qj,k)
+   G(:, :, x) = round(G(:, :, x)./Q);
    
-		DC = [DC; G(1, 1, x)];
-		AC = [AC; [runLength(zigzag(G(:, :, x)))].'];
-	end
+   DC = [DC; G(1, 1, x)];
+   AC = [AC; [runLength(zigzag(G(:, :, x)))].'];
+end
 
 
-	% Faz a compressão DPCM dos coeficientes DC
-	% Isto codifica cada coeficiente DC como um delta do coeficiente anterior
-	DC = DPCM(DC);
+% Faz a compressão DPCM dos coeficientes DC
+% Isto codifica cada coeficiente DC como um delta do coeficiente anterior
+DC = DPCM(DC);
 
-	% Coloca numa estrutura os dados comprimidos e a info relevante
-	jpeg = struct('field_q', q,...
-			   'field_sizeI', size(I), ...
-            'field_sizeG', size(G),...
-            'field_DC', DC,...
-            'field_AC', AC);
-         
-         
-         
-         
-
-	%%%%%%%%%%%%% FUNÇÕES %%%%%%%%%%%%%%%%
-   
-% Função DCT 2D
-function retorno = myDCT2(bloco)
-	tamanhoBloco= 8;
-	retorno = zeros(tamanhoBloco, tamanhoBloco);
-
-	for u = 0 : tamanhoBloco-1
-   	for v = 0 : tamanhoBloco-1
-      	if u == 0
-         	alphaU = sqrt(1/8);
-      	else
-         	alphaU = sqrt(2/8);
-      	end
-      
-      	if v == 0
-         	alphaV = sqrt(1/8);
-      	else
-         	alphaV = sqrt(2/8);
-      	end
-      
-      	somatorio = 0;
-      
-      	for x = 0 : tamanhoBloco-1
-         	for y = 0 : tamanhoBloco-1
-            	somatorio = somatorio + ...
-            				(bloco(x+1, y+1) * ...
-            				cos((pi/8) * (x + 1/2) * u) * ...
-            				cos((pi/8) * (y + 1/2) * v));
-         	end
-      	end
-      
-      	retorno(u+1, v+1) = alphaU * alphaV * somatorio;
-   	end
-	end
-
-  
-% 3. Mostre que existe uma relação entre a distorção introduzida pela quantização (e.g. usando PSNR)
-%		e o número de bits usados na quantização dos coeficientes da DCT
-% ---------------------------------------------------------------------------------
-% calc_snr - calculates the snr of a figure being compressed
-%
-% assumption: SNR calculation is done in the following manner:
-%             the deviation from the original image is considered 
-%             to be the noise therefore:
-%
-%                   noise = original_image - compressed_image
-%
-%             the SNR is defined as:  
-%
-%                   SNR = energy_of_image/energy_of_noise
-%
-%             which yields: 
-%
-%                   SNR = energy_of_image/((original_image-compressed_image)^2)
-% ---------------------------------------------------------------------------------
-function SNR = calc_snr(original_image, noisy_image)
-	original_image_energy = sum( original_image(:).^2 );
-	noise_energy = sum( (original_image(:)-noisy_image(:)).^2 );
-	SNR = original_image_energy/noise_energy;
-
-% 4. Para cada img use uma fator multiplicativo p/ a Tabela de Quantização "q" (q >= 1)
-%		p/ mudar os passos de quantização especs na Tabela.
+% Coloca numa estrutura os dados comprimidos e a info relevante
+jpeg = struct('field_q', q,...
+   'field_sizeI', size(I), ...
+   'field_sizeG', size(G),...
+   'field_DC', DC,...
+   'field_AC', AC);
 
 return
--- runLength.m
+++ runLength.m
@@ -1,32 +1,43 @@
-function resp  =  runLength(v)
-% Elimina corridas de zeros no vetor v
-% Utiliza a seguinte sintaxe:
+% Elimina 0's no vetor v
+% RunLength / Cod. Huffman
 
-%Simbolo que marca o fim do bloco.
-EOB = 255;
+function ret = runLength(AC)
+EOB = 255;		% Símbolo End-Of-Block
+iZeros = 0;		% contador dos zeros (n)
+pos = 1;			% Indíce atual no vetor de restorno
 
-% incicia a contagem dos zeros do vetor
-nzeros = 0;
-k = 1;
-for i = 1:size(v,2);
-   if v(i) == 0
-		nzeros = nzeros+1;
+
+for i = 1 : size(AC, 2);
+   
+   % vai incrementando os zeros
+   if AC(i) == 0
+		iZeros = iZeros + 1;
    else
       % quando o elemento for diferente de zero
       % atribui os zeros à posiçaõ e o elemento à seguinte
-		resp(k) = nzeros;
-		resp(k+1) = v(i);
-		k = k+2;
-		nzeros = 0;
+      % (1)  O si´mbolo (n) denota quantos zeros precedem um AC na~o-nulo;
+      
+      ret(pos) = iZeros;
+      
+      % (2) O número (ex: 7, 1 ou 19) é a palavra-símbolo que representa a corrida de 
+      % zeros, e a distribuição de coeficientes quantizados será agora representada 
+      % pela distribuição de palavras-símbolo – ou seja, você terá um dicionário de 
+      % palavras-símbolo e uma distribuição das suas ocorrências nas corridas de 
+      % todos os blocos da imagem;
+      
+      ret(pos + 1) = AC(i);
+      
+      pos = pos + 2;
+		iZeros = 0;
 	end
 end
 
-% verifica a colocação do último marcador
-if (nzeros  == 0)
-   resp(k) = EOB;
-else
-   resp(k) = nzeros;
-   resp(k+1) = EOB;
+if (iZeros > 0)
+   ret(pos) = iZeros;
+	pos = pos + 1
 end
+
+% (3) O símbolo EOB (end of block) representa o final do bloco
+ret(pos) = EOB;
 
 return
